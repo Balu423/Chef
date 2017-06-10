91469801901a2fd8b2728120ec6a8aaed7348503
root <root@ip-172-31-23-127.ap-south-1.compute.internal>
2017-06-10 09:47:14 +0000

    cookbooks

---
+++ cookbooks/attributes/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: attributes
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package node["package_name"] do
+action :install
+end
+
+service node["service_name"] do
+action [:enable, :start]
+end
+
+template "#{node["document_root"]}/index.html" do
+source "index.html.erb"
+end
